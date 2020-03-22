7469129663e367a35686cb4fcd06928f986b2cac
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2020-03-22 14:37:34 -0400

    send hostname

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import * as express from 'express'
+import * as os from "os"
 import { Application } from 'express'
 const app = express()
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+var count = 0;
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send("OK")
+  res.json({ Hostname: os.hostname(), "count": ++count });
 })
 app.listen(port, () => {
   console.log("Listening")
