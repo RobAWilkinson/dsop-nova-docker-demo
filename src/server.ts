81803c4227c021432eb76fffde39db7069e1d4e5
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2020-03-22 15:16:41 -0400

    connected to redis

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import * as express from 'express'
+import * as redis from "redis";
 import * as os from "os"
+import { promisify } from "util";
 import { Application } from 'express'
 const app = express()
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-var count = 0;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.json({ Hostname: os.hostname(), "count": ++count });
+const redisConfig = {
+  url: process.env.REDIS_URL ||"redis://localhost",
+}
+const client = redis.createClient(redisConfig);
+var getAsync = promisify(client.get).bind(client);
+var setAsync = promisify(client.set).bind(client);
+
+app.get('/', async (req: express.Request, res: express.Response) => {
+  var count: number = await getAsync("count");
+
+  await setAsync('count', ++count );
+  res.json({ Hostname: os.hostname(), "count": count });
 })
 app.listen(port, () => {
   console.log("Listening")
